07c32261a42a1a64cee6f2c18449c092288343fa
HitoTei <43402618+HitoTei@users.noreply.github.com>
2023-09-13 11:24:34 +0900

    Update infromation.tsx

--- src/ui/information/infromation.tsx
+++ src/ui/information/infromation.tsx
@@ -10,7 +10,7 @@ export default class Information extends React.Component{
         titles.set('Interest' , ['Web', 'RL', 'ANN', ])
         titles.set('Qualification' , ['基本情報技術者試験', "応用情報技術者試験", "TOEIC 775"])
         titles.set('Intern' , ['Cybozu Android2week (2023 8/21 ~ 9/1)'])
-        titles.set('Works' , ['githubみて'])
+        titles.set('Works' , ['githubみて！'])
         
         const tileList: Array<React.ReactElement> = []
         titles.forEach(
